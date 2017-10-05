db7cae721321b0fc0a546defef415bac15c36afa
Tom White <tw@commercialsoftware.co.uk>
2017-10-05 06:49:04 +0100

    Refactor: Interface added with Max and Gordon So I don't have to hack the Type anymore

--- src/app/effects/count-effects.service.ts
+++ src/app/effects/count-effects.service.ts
@@ -4,15 +4,26 @@ import 'rxjs/add/operator/catch';
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
-import { Action, Store } from '@ngrx/store';
 import { Actions, Effect } from '@ngrx/effects';
 import { of } from 'rxjs/observable/of';
+import { Store } from '@ngrx/store';
+
+export interface Action {
+  type: string;
+  payload?: any;
+}
+
+// // https://github.com/ngrx/platform/issues/128#issuecomment-316680916
+// export interface ActionWithPayload<T> extends Action {
+//   payload: T;
+// }
+
 
 @Injectable()
 export class CountEffects {
   // Listen for the 'RESET' action
   @Effect() reset$: Observable<Action> = this.actions$.ofType('RESET')
-    .mergeMap(action => {
+    .mergeMap((action: Action) => {
       console.log('inside CountEffects');
       console.log('Counter value before reset: ', action.payload);
       return of({ type: 'RESETTED', payload: 'ha!' });
@@ -21,7 +32,7 @@ export class CountEffects {
 
   // Listen for the 'RESETTED' action
   @Effect() resetted$: Observable<Action> = this.actions$.ofType('RESETTED')
-    .mergeMap(action => {
+    .mergeMap((action: Action) => {
       console.log(action.payload, 'Counter has been resetted!');
       return of({ type: 'AFTER-RESETTED', payload: action.payload });
     }
@@ -39,14 +50,3 @@ export class CountEffects {
 
 
 
-
-
-// https://github.com/ngrx/platform/issues/128#issuecomment-316680916
-export interface ActionWithPayload<T> extends Action {
-  payload: T;
-}
-// didn't work so, hacked in node_modules\@ngrx\store\models.d.ts:
-export interface Action {
-  type: string;
-  payload?: any; // hacked in by Tom
-}
